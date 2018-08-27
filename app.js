1b487c7e5ae20d6f644a46579c093a7a8f087e44
Brandon Watson <brandonewatson@gmail.com>
2018-08-27 15:28:59 -0700

    Image Attempt

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ function func(){
 
 	app.listen(3000, () => console.log('Example app listening on port 3000!'))
 }
+
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
+}
+
+<button onclick=
+    "show_image('http://google.com/images/logo.gif', 
+                 276, 
+                 110, 
+                 'Google Logo');">Add Google Logo</button> 
